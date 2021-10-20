5d49adfd518aeb05b8abb75e343e672f5aa8b3a9
leoskrr <vianaleonardo.es@gmail.com>
2021-10-20 10:01:07 -0400

    delete storyboard from project

---
+++ drawAI/Scenes/HomeViewController/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  drawAI
+//
+//  Created by Leonardo Viana on 20/10/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

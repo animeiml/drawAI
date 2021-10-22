602c6497bfe572c5ea942132ea0ade6d50490afc
guilherme <jguicf@gmail.com>
2021-10-22 11:34:52 -0400

    basic ui and fonts implemented

--- drawAI/SceneDelegate.swift
+++ drawAI/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         window?.frame = UIScreen.main.bounds
-        window?.rootViewController = HomeViewController()
+        window?.rootViewController = EndViewController()
         window?.makeKeyAndVisible()
     }
 
---
+++ drawAI/Scenes/EndScene/EndViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EndViewController.swift
+//  drawAI
+//
+//  Created by João Guilherme on 22/10/21.
+//
+
+import UIKit
+
+class EndViewController: UIViewController {
+
+    @IBOutlet weak var userDraw: UIView!
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        userDraw.layer.cornerRadius = 22
+        homeButton.layer.cornerRadius = 23
+        saveButton.layer.cornerRadius = 23
+        nextButton.layer.cornerRadius = 23
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

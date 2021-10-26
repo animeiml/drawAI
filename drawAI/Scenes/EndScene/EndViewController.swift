293bc6c3462c387e764f32873a5bb07efc9ff6c0
guilherme <jguicf@gmail.com>
2021-10-26 09:04:01 -0400

    feat: add image test an implemented autolayout

--- drawAI/Scenes/EndScene/EndViewController.swift
+++ drawAI/Scenes/EndScene/EndViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class EndViewController: UIViewController {
 
+    @IBOutlet weak var userDrawImage: UIImageView!
+    @IBOutlet weak var referenceImage: UIImageView!
+    @IBOutlet weak var imageView: UIView!
     @IBOutlet weak var scoreView: UIView!
     @IBOutlet weak var userDraw: UIView!
     @IBOutlet weak var homeButton: UIButton!
@@ -18,14 +21,31 @@ class EndViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        userDraw.layer.cornerRadius = 22
-        homeButton.layer.cornerRadius = 23
-        saveButton.layer.cornerRadius = 23
-        nextButton.layer.cornerRadius = 23
+        userDraw.layer.cornerRadius = userDraw.frame.height * 0.08
+        homeButton.layer.cornerRadius = homeButton.frame.height / 2
+        saveButton.layer.cornerRadius = saveButton.frame.height / 2
+        nextButton.layer.cornerRadius = nextButton.frame.height / 2
+        
+        var config = UIButton.Configuration.filled()
+        config.imagePlacement = .trailing
+        config.imagePadding = 5
+        
+        nextButton.configuration?.imagePadding = nextButton.frame.width * 0.4391
         
         scoreView.layer.borderColor = UIColor(red: 0.00, green: 0.48, blue: 1.00, alpha: 0.20).cgColor
         scoreView.layer.borderWidth = 1
         scoreView.layer.cornerRadius = scoreView.frame.width / 2
+        
+        imageView.layer.cornerRadius = imageView.frame.height * 0.275
+        imageView.layer.borderColor = UIColor(red: 0.00, green: 0.48, blue: 1.00, alpha: 0.20).cgColor
+        imageView.layer.borderWidth = 1
+        
+        referenceImage.image = UIImage(named: "kakashi")
+        referenceImage.contentMode = .scaleAspectFit
+        referenceImage.layer.cornerRadius = imageView.frame.height * 0.275
+        
+        userDrawImage.contentMode = .scaleAspectFit
+        userDrawImage.layer.cornerRadius = userDraw.frame.height * 0.08
     }
 
 

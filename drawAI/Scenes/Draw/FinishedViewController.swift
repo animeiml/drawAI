557c0e1eff1d9ad514f0bb6fe37943bec7f871f7
guilherme <jguicf@gmail.com>
2021-10-26 09:37:31 -0400

    feat: add new fonts and implemente the finishView

---
+++ drawAI/Scenes/Draw/FinishedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FinishedViewController.swift
+//  drawAI
+//
+//  Created by João Guilherme on 26/10/21.
+//
+
+import UIKit
+
+class FinishedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

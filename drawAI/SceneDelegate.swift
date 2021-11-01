c9ad2feb5c725550a061f712999d6dc52541c0d8
guilherme <jguicf@gmail.com>
2021-11-01 08:22:16 -0400

    feat: add archives to project

--- drawAI/SceneDelegate.swift
+++ drawAI/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let homeViewController : HomeViewController = HomeViewController()
+        let homeViewController : HomeIpadViewController = HomeIpadViewController()
 
         let navController: UINavigationController = UINavigationController(rootViewController: homeViewController)
                 
---
+++ drawAI/Scenes/HomeViewController/Ipad/HomeIpadViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeIpadViewController.swift
+//  drawAI
+//
+//  Created by João Guilherme on 29/10/21.
+//
+
+import UIKit
+
+class HomeIpadViewController: UIViewController {
+
+    @IBOutlet weak var logoView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //logoView.layer.cornerRadius = logoView.frame.width * 0.14
+        //startButton.layer.cornerRadius = startButton.frame.height/2
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

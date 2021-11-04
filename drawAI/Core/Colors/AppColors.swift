9ab03efc461e736d92cea614191a7798de2c2324
leoskrr <vianaleonardo.es@gmail.com>
2021-11-03 21:29:30 -0400

    create color tokens

---
+++ drawAI/Core/Colors/AppColors.swift
@@ -0,0 +1,29 @@
+//
+//  AppColor.swift
+//  drawAI
+//
+//  Created by Leonardo Viana on 03/11/21.
+//
+
+import Foundation
+import UIKit
+
+struct AppColors {
+    /// Color with hex #FFF7F0
+    static let backgroundColor: UIColor = UIColor(rgb: 0xFFF7F0)
+    
+    /// Color with hex #224EED
+    static let primaryColor: UIColor = UIColor(rgb: 0x224EED)
+    
+    /// Color with hex #B4F474
+    static let accentColor: UIColor = UIColor(rgb: 0xB4F474)
+    
+    /// Color with hex #F57C7C
+    static let attentionColor: UIColor = UIColor(rgb: 0xF57C7C)
+    
+    /// Color with hex #797A7B
+    static let disabledColor: UIColor = UIColor(rgb: 0x797A7B)
+    
+    /// Color with hex #3C3733
+    static let iconColor: UIColor = UIColor(rgb: 0x3C3733)
+}

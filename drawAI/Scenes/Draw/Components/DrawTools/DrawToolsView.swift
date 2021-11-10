6d7aba81e37df462e3c0fa4bfe177c9c8af7bcd9
guilherme <jguicf@gmail.com>
2021-11-10 10:02:35 -0400

    feat: add disable and enable in draw tools

--- drawAI/Scenes/Draw/Components/DrawTools/DrawToolsView.swift
+++ drawAI/Scenes/Draw/Components/DrawTools/DrawToolsView.swift
@@ -21,18 +21,52 @@ class DrawToolsView: ANView {
     
     override func awakeFromNib() {
         debugPrint("==== \(pencilButton.frame) ====")
+        
+        pencilButton.tintColor = AppColors.primaryColor.withAlphaComponent(0.15)
+        pencilButton.layer.borderColor = AppColors.primaryColor.withAlphaComponent(0.20).cgColor
+        pencilButton.layer.borderWidth = 1
+        pencilButton.setImage(UIImage(named: "pencil"), for: .normal)
+    
+        eraserButton.tintColor = AppColors.disabledColor.withAlphaComponent(0.15)
+        eraserButton.layer.borderColor = AppColors.disabledColor.withAlphaComponent(0.20).cgColor
+        eraserButton.layer.borderWidth = 1
+        eraserButton.setImage(UIImage(named: "eraser-disable"), for: .normal)
+        
 
         pencilButton.setRounded()
         eraserButton.setRounded()
+        
+        
 
         super.awakeFromNib()
     }
     
     @IBAction func onPencilTouched(_ sender: Any) {
         delegate?.onPencilChosen()
+        
+        pencilButton.tintColor = AppColors.primaryColor.withAlphaComponent(0.15)
+        pencilButton.layer.borderColor = AppColors.primaryColor.withAlphaComponent(0.20).cgColor
+        pencilButton.layer.borderWidth = 1
+        pencilButton.setImage(UIImage(named: "pencil"), for: .normal)
+        
+        eraserButton.tintColor = AppColors.disabledColor.withAlphaComponent(0.15)
+        eraserButton.layer.borderColor = AppColors.disabledColor.withAlphaComponent(0.20).cgColor
+        eraserButton.layer.borderWidth = 1
+        eraserButton.setImage(UIImage(named: "eraser-disable"), for: .normal)
+        
     }
     
     @IBAction func onEraserTouched(_ sender: Any) {
         delegate?.onEraserChosen()
+        
+        pencilButton.tintColor = AppColors.disabledColor.withAlphaComponent(0.15)
+        pencilButton.layer.borderColor = AppColors.disabledColor.withAlphaComponent(0.20).cgColor
+        pencilButton.layer.borderWidth = 1
+        pencilButton.setImage(UIImage(named: "pencil-disable"), for: .normal)
+        
+        eraserButton.tintColor = AppColors.primaryColor.withAlphaComponent(0.15)
+        eraserButton.layer.borderColor = AppColors.primaryColor.withAlphaComponent(0.20).cgColor
+        eraserButton.layer.borderWidth = 1
+        eraserButton.setImage(UIImage(named: "eraser"), for: .normal)
     }
 }
